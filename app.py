04e553626899d9bcd709b8e5e7667252728eaca7
hollierjm <hollierjm@gmail.com>
2023-12-08 16:21:44 -0800

    cleaned up and matched format

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 "Module providing employee service"
-import os
 from flask import Flask, request, jsonify
-import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-
-#app.config["MYSQL_DATABASE_USER"] = "root"
-#app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
-#app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
-#app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
-#app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
-app.config ['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@mysql:3306/employees"
+app.config ['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@mysql:3306/employeedb"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 "Employee class"
-class Employee(db.Model):
+class Employees(db.Model):
     id = db.Column('EmployeeID', db.Integer, primary_key = True)
     first_name = db.Column('FirstName', db.String(25))
     last_name = db.Column('LastName', db.String(25))
     email_addr = db.Column('EmailAddress', db.String(50))
-    country = db.Column('Country', db.Integer)
+    country = db.Column('Country', db.String(2))
     
     def __init__(self, first_name, last_name, email_addr, country):
         self.first_name = first_name
@@ -32,19 +24,19 @@ class Employee(db.Model):
         self.country = country
 
 #Create schema to display the Employee object
-class EmployeeSchema(ma.Schema):
+class EmployeesSchema(ma.Schema):
     class Meta:
         fields = ('id', 'first_name', 'last_name', 'email_addr', 'country')
 
-employee_schema = EmployeeSchema() #singular Employee
-employees_schema = EmployeeSchema(many=True) #multiple Employees
+employee_schema = EmployeesSchema() #singular Employee
+employees_schema = EmployeesSchema(many=True) #multiple Employees
 
 @app.route('/')
 def display_peoplesuite():
     return 'Displaying PeopleSuite'
 
 #Create new employee from JSON request and add to table
-@app.route('/employee', methods=['POST'])
+@app.route('/employees', methods=['POST'])
 def add_employee():
     try:
         first_name = request.json['FirstName']
@@ -52,26 +44,32 @@ def add_employee():
         email_addr = request.json['EmailAddress']
         country = request.json['Country']
 
-        new_employee = Employee(first_name, last_name, email_addr, country)
+        new_employee = Employees(first_name, last_name, email_addr, country)
         db.session.add(new_employee)
         db.session.commit()
 
-        return employee_schema.jsonify(new_employee)
-    except:
-        return employee_schema.jsonify(new_employee)
+        return employee_schema.jsonify(new_employee), 200
+    except Exception as e:
+        return e
 
 #Return all Employees in table
-@app.route('/employee', methods=['GET'])
+@app.route('/employees', methods=['GET'])
 def get_employees():
-    all_employees = Employee.query.all()
-    result = employees_schema.dump(all_employees)
-    return jsonify(result)
+    try:
+        all_employees = Employees.query.all()
+        result = employees_schema.dump(all_employees)
+        return jsonify(result), 200
+    except Exception as e:
+        return e
 
 #Return employee with specified EmployeeID if possible
-@app.route('/employee/<id>', methods=['GET'])
+@app.route('/employees/<id>', methods=['GET'])
 def get_employee(id):
-    employee = Employee.query.get(id)
-    return employee_schema.jsonify(employee)
+    try:
+        employee = Employees.query.get(id)
+        return employee_schema.jsonify(employee), 200
+    except Exception as e:
+        return e
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
